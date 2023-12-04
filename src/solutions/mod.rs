35b21d0500981ffbbe32acf58ac8625d002fe883
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-12-04 13:53:38 +0530

    feat: day 4 solutions

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,12 +1,14 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 pub fn get_solution(day: u32, part: u32) {
     match day {
         1 => day1::solution(part),
         2 => day2::solution(part),
         3 => day3::solution(part),
+        4 => day4::solution(part),
         _ => println!("Invalid day."),
     }
 }
