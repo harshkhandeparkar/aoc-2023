f258afcb39153e832b9a7c8f25c5ae8951d537f1
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-12-06 00:01:05 +0530

    fmt too

--- src/solutions/day5.rs
+++ src/solutions/day5.rs
@@ -1,77 +1,88 @@
 pub fn solution(part: u32) {
-	let input = get_input();
+    let input = get_input();
 
-	let min_location = input.seeds.iter().map(|seed| {
-		let mut location = *seed;
+    let min_location = input
+        .seeds
+        .iter()
+        .map(|seed| {
+            let mut location = *seed;
 
-		for map in input.maps.clone() {
-			for range in map {
-				if range.src_start <= location && location <= range.src_end {
-					location = range.dst_start + location - range.src_start;
-					break;
-				}
-			}
-		}
+            for map in input.maps.clone() {
+                for range in map {
+                    if range.src_start <= location && location <= range.src_end {
+                        location = range.dst_start + location - range.src_start;
+                        break;
+                    }
+                }
+            }
 
-		location
-	}).min().unwrap();
+            location
+        })
+        .min()
+        .unwrap();
 
-	if part == 1 {
-		println!("{}", min_location);
-	} else {
-		// Check if any locations less than min_location are feasible and return that
-		let mut best_known_min_location = min_location;
+    if part == 1 {
+        println!("{}", min_location);
+    } else {
+        // Check if any locations less than min_location are feasible and return that
+        let mut best_known_min_location = min_location;
 
-		for test_location in 0..min_location {
-			println!("testing {}%", (test_location as f64 / min_location as f64) * 100.0);
-			let mut traversed_location = test_location;
-			let mut new_best_found = false;
-			// Check feasibility `by applying the inverse transform
-			for map in input.maps.clone().iter().rev() {
-				for range in map {
-					if range.dst_start <= traversed_location && traversed_location <= range.dst_end {
-						traversed_location = range.src_start + traversed_location - range.dst_start;
-						break;
-					}
-				}
-			}
+        for test_location in 0..min_location {
+            println!(
+                "testing {}%",
+                (test_location as f64 / min_location as f64) * 100.0
+            );
+            let mut traversed_location = test_location;
+            let mut new_best_found = false;
+            // Check feasibility `by applying the inverse transform
+            for map in input.maps.clone().iter().rev() {
+                for range in map {
+                    if range.dst_start <= traversed_location && traversed_location <= range.dst_end
+                    {
+                        traversed_location = range.src_start + traversed_location - range.dst_start;
+                        break;
+                    }
+                }
+            }
 
-			let mut seed_iter = input.seeds.iter();
-			while let Some(seed_start) = seed_iter.next() {
-				if let Some(seed_len) = seed_iter.next() {
-					if *seed_start <= traversed_location && traversed_location < *seed_start + *seed_len {
-						best_known_min_location = traversed_location;
-						new_best_found = true;
-						break;
-					}
-				}
-			}
+            let mut seed_iter = input.seeds.iter();
+            while let Some(seed_start) = seed_iter.next() {
+                if let Some(seed_len) = seed_iter.next() {
+                    if *seed_start <= traversed_location
+                        && traversed_location < *seed_start + *seed_len
+                    {
+                        best_known_min_location = traversed_location;
+                        new_best_found = true;
+                        break;
+                    }
+                }
+            }
 
-			if new_best_found {
-				break;
-			}
-		}
+            if new_best_found {
+                break;
+            }
+        }
 
-		println!("{}", best_known_min_location);
-	}
+        println!("{}", best_known_min_location);
+    }
 }
 
 #[derive(Debug, Clone)]
 struct ParsedInput {
-	seeds: Vec<u64>,
-	maps: Vec<Vec<Range>>,
+    seeds: Vec<u64>,
+    maps: Vec<Vec<Range>>,
 }
 
 #[derive(Debug, Clone, Copy)]
 struct Range {
-	src_start: u64,
-	src_end: u64,
-	dst_start: u64,
-	dst_end: u64,
+    src_start: u64,
+    src_end: u64,
+    dst_start: u64,
+    dst_end: u64,
 }
 
 fn get_input() -> ParsedInput {
-	let raw_input = "seeds: 4088478806 114805397 289354458 164506173 1415635989 166087295 1652880954 340945548 3561206012 483360452 35205517 252097746 1117825174 279314434 3227452369 145640027 2160384960 149488635 2637152665 236791935
+    let raw_input = "seeds: 4088478806 114805397 289354458 164506173 1415635989 166087295 1652880954 340945548 3561206012 483360452 35205517 252097746 1117825174 279314434 3227452369 145640027 2160384960 149488635 2637152665 236791935
 
 seed-to-soil map:
 3333452986 2926455387 455063168
@@ -281,26 +292,43 @@ humidity-to-location map:
 2965556648 2811971059 154696004
 3236840747 3205935028 252772539";
 
-	let seeds = raw_input.lines().next().unwrap().split_once(':').unwrap().1.split_whitespace().map(|num| num.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+    let seeds = raw_input
+        .lines()
+        .next()
+        .unwrap()
+        .split_once(':')
+        .unwrap()
+        .1
+        .split_whitespace()
+        .map(|num| num.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>();
 
-	let maps = raw_input.split_once("\n\n").unwrap().1.split("\n\n").map(|map| map.split_once(':').unwrap().1.trim()).map(|map| {
-		map.lines().map(|line| {
-			let mut range_iter = line.split_whitespace().map(|num| num.parse::<u64>().unwrap());
-			let dst_start = range_iter.next().unwrap();
-			let src_start = range_iter.next().unwrap();
-			let length = range_iter.next().unwrap();
+    let maps = raw_input
+        .split_once("\n\n")
+        .unwrap()
+        .1
+        .split("\n\n")
+        .map(|map| map.split_once(':').unwrap().1.trim())
+        .map(|map| {
+            map.lines()
+                .map(|line| {
+                    let mut range_iter = line
+                        .split_whitespace()
+                        .map(|num| num.parse::<u64>().unwrap());
+                    let dst_start = range_iter.next().unwrap();
+                    let src_start = range_iter.next().unwrap();
+                    let length = range_iter.next().unwrap();
 
-			Range {
-				src_start,
-				src_end: src_start + length - 1,
-				dst_start,
-				dst_end: dst_start + length - 1
-			}
-		}).collect::<Vec<Range>>()
-	}).collect::<Vec<Vec<Range>>>();
+                    Range {
+                        src_start,
+                        src_end: src_start + length - 1,
+                        dst_start,
+                        dst_end: dst_start + length - 1,
+                    }
+                })
+                .collect::<Vec<Range>>()
+        })
+        .collect::<Vec<Vec<Range>>>();
 
-	ParsedInput {
-		seeds,
-		maps
-	}
+    ParsedInput { seeds, maps }
 }
