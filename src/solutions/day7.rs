4bd6632166302e42f4fe916b0f0e604503ecf903
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-12-08 20:38:23 +0530

    feat: day 7, part 2

--- src/solutions/day7.rs
+++ src/solutions/day7.rs
@@ -1,19 +1,15 @@
 use std::{cmp::Ordering, collections::HashMap};
 
 pub fn solution(part: u32, custom_solution: Option<&str>) -> u128 {
-    let mut input = get_input(custom_solution);
+    let mut input = get_input(custom_solution, part);
 
-    if part == 1 {
-        input.sort_by(|a, b| a.cmp(b));
+    input.sort_by(|a, b| a.cmp(b, part));
 
-        input
-            .iter()
-            .enumerate()
-            .map(|(i, hand)| (i as u32 + 1) * hand.bid)
-            .sum::<u32>() as u128
-    } else {
-        0
-    }
+    input
+        .iter()
+        .enumerate()
+        .map(|(i, hand)| (i as u32 + 1) * hand.bid)
+        .sum::<u32>() as u128
 }
 
 #[test]
@@ -35,6 +31,27 @@ QQQJA 483"
 #[test]
 fn check_part1() {
     assert_eq!(solution(1, None), 250254244)
+}
+
+#[test]
+fn check_example_part2() {
+    assert_eq!(
+        solution(
+            2,
+            "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"
+                .into()
+        ),
+        5905
+    )
+}
+
+#[test]
+fn check_part2() {
+    assert_eq!(solution(2, None), 250087440)
 }
 
 #[derive(Debug)]
@@ -64,7 +81,7 @@ impl HandType {
 }
 
 impl Hand {
-    pub fn new(cards_str: &str, bid: u32) -> Hand {
+    pub fn new(cards_str: &str, bid: u32, part: u32) -> Hand {
         Hand {
             card_values: cards_str
                 .chars()
@@ -72,7 +89,13 @@ impl Hand {
                     'A' => 14,
                     'K' => 13,
                     'Q' => 12,
-                    'J' => 11,
+                    'J' => {
+                        if part == 1 {
+                            11
+                        } else {
+                            0
+                        }
+                    }
                     'T' => 10,
                     _ => character.to_digit(10).unwrap(),
                 })
@@ -81,7 +104,8 @@ impl Hand {
         }
     }
 
-    pub fn get_type(&self) -> HandType {
+    // returns handtype and number of jokers
+    pub fn get_type(&self, part: u32) -> HandType {
         let mut card_nums = HashMap::<u32, u32>::new();
 
         for card in &self.card_values {
@@ -92,31 +116,75 @@ impl Hand {
             }
         }
 
+        let joker_value = if part == 1 { 11 } else { 0 };
+        let num_jokers = *card_nums.get(&joker_value).unwrap_or(&0);
         let card_nums: Vec<u32> = card_nums.values().copied().collect();
 
         match card_nums.len() {
             1 => HandType::FiveOfAKind,
             2 => {
-                if card_nums.contains(&4) || card_nums.contains(&1) {
-                    HandType::FourOfAKind
+                if card_nums.contains(&4) {
+                    // 4,1
+                    if num_jokers > 0 && part == 2 {
+                        HandType::FiveOfAKind
+                    } else {
+                        HandType::FourOfAKind
+                    }
                 } else {
-                    HandType::FullHouse
+                    // 3,2
+                    if part == 2 && num_jokers > 0 {
+                        if num_jokers == 1 {
+                            HandType::FourOfAKind
+                        } else {
+                            HandType::FiveOfAKind
+                        }
+                    } else {
+                        HandType::FullHouse
+                    }
                 }
             }
             3 => {
                 if card_nums.contains(&3) {
-                    HandType::ThreeOfAKind
+                    // 3,1,1
+                    if part == 2 && num_jokers > 0 {
+                        HandType::FourOfAKind
+                    } else {
+                        HandType::ThreeOfAKind
+                    }
                 } else {
-                    HandType::TwoPair
+                    // 2,2,1
+                    if part == 2 && num_jokers > 0 {
+                        if num_jokers == 1 {
+                            HandType::FullHouse
+                        } else {
+                            HandType::FourOfAKind
+                        }
+                    } else {
+                        HandType::TwoPair
+                    }
                 }
             }
-            4 => HandType::OnePair,
-            _ => HandType::HighCard,
+            4 => {
+                // 2,1,1,1
+                if part == 2 && num_jokers > 0 {
+                    HandType::ThreeOfAKind
+                } else {
+                    HandType::OnePair
+                }
+            }
+            _ => {
+                // 1,1,1,1,1
+                if part == 2 && num_jokers > 0 {
+                    HandType::OnePair
+                } else {
+                    HandType::HighCard
+                }
+            }
         }
     }
 
-    pub fn cmp(&self, hand2: &Hand) -> Ordering {
-        match self.get_type().cmp(&hand2.get_type()) {
+    pub fn cmp(&self, hand2: &Hand, part: u32) -> Ordering {
+        match self.get_type(part).cmp(&hand2.get_type(part)) {
             Ordering::Equal => {
                 for (i, &card_value1) in self.card_values.iter().enumerate() {
                     let card_value2 = hand2.card_values[i];
@@ -134,9 +202,23 @@ impl Hand {
     }
 }
 
-fn get_input(custom_solution: Option<&str>) -> Vec<Hand> {
-    let raw_input = custom_solution.unwrap_or(
-        "Q97J7 740
+fn get_input(custom_solution: Option<&str>, part: u32) -> Vec<Hand> {
+    let raw_input = custom_solution.unwrap_or(get_raw_input());
+
+    raw_input
+        .lines()
+        .map(|line| {
+            Hand::new(
+                line.split_once(' ').unwrap().0,
+                line.split_once(' ').unwrap().1.parse::<u32>().unwrap(),
+                part,
+            )
+        })
+        .collect()
+}
+
+fn get_raw_input() -> &'static str {
+    "Q97J7 740
 65KJ6 889
 6664J 275
 JKK44 856
@@ -1135,16 +1217,5 @@ J898K 448
 J6ATJ 910
 K59AK 18
 TK3TJ 642
-888JT 476",
-    );
-
-    raw_input
-        .lines()
-        .map(|line| {
-            Hand::new(
-                line.split_once(' ').unwrap().0,
-                line.split_once(' ').unwrap().1.parse::<u32>().unwrap(),
-            )
-        })
-        .collect()
+888JT 476"
 }
