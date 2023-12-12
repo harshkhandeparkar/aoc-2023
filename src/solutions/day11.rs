b752cbe77daffaf103fbb59f58be60084b762f4d
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-12-12 22:32:57 +0530

    feat: day11 done

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -1,3 +1,64 @@
+use crate::inputs::day11::PUZZLE_INPUT;
+
 pub fn solution(part: u32, custom_input: Option<&str>) -> i128 {
-	0
+	let expansion_rate: i128 = if part == 1 {
+		2
+	} else {
+		1000_000
+	};
+
+    let (_, galaxy_locations, empty_row_indexes, empty_col_indexes) = get_input(custom_input);
+
+	galaxy_locations.iter().enumerate().map(|(i, loc1)| {
+		galaxy_locations[(i+1)..].iter().map(|loc2| {
+			let i_min = std::cmp::min(loc1.0, loc2.0);
+			let i_max = std::cmp::max(loc1.0, loc2.0);
+
+			let j_min = std::cmp::min(loc1.1, loc2.1);
+			let j_max = std::cmp::max(loc1.1, loc2.1);
+
+			let num_expanded_rows = empty_row_indexes.iter().filter(|&&i| i_min < i && i < i_max).count() as i128;
+			let num_expanded_cols = empty_col_indexes.iter().filter(|&&j| j_min < j && j < j_max).count() as i128;
+
+			(i_max - i_min) as i128 + (j_max - j_min) as i128 + (num_expanded_rows + num_expanded_cols) * (expansion_rate - 1)
+		}).sum::<i128>()
+	}).sum::<i128>()
+}
+
+fn get_input(custom_input: Option<&str>) -> (Vec<Vec<char>>, Vec<(usize, usize)>, Vec<usize>, Vec<usize>) {
+    let raw_input = custom_input.unwrap_or(PUZZLE_INPUT);
+    let cosmos: Vec<Vec<char>> = raw_input
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+
+    let expanded_row_indexes: Vec<usize> = cosmos
+        .iter()
+        .enumerate()
+        .filter(|(_, line)| line.iter().all(|&tile| tile == '.'))
+        .map(|(i, _)| i)
+        .collect();
+
+    let mut expanded_col_indexes: Vec<usize> = Vec::new();
+    for i in 0..cosmos[0].len() {
+        if cosmos.iter().all(|line| line[i] == '.') {
+            expanded_col_indexes.push(i);
+        }
+    }
+
+
+    let galaxy_locations: Vec<(usize, usize)> = cosmos
+        .iter()
+        .enumerate()
+        .map(|(i, line)| {
+            line.iter()
+                .enumerate()
+                .filter(|(_, &tile)| tile == '#')
+                .map(|(j, _)| (i, j))
+                .collect::<Vec<(usize, usize)>>()
+        })
+        .flatten()
+        .collect();
+
+    (cosmos, galaxy_locations, expanded_row_indexes, expanded_col_indexes)
 }
--- src/test/day11.rs
+++ src/test/day11.rs
@@ -1,106 +1,32 @@
 use crate::solutions::day11::solution;
 
 #[test]
-fn check_part1_example1() {
+fn check_part1_example() {
     assert_eq!(
         solution(
             1,
-            ".....
-.S-7.
-.|.|.
-.L-J.
-....."
+            "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."
                 .into()
         ),
-        4
-    );
-}
-
-#[test]
-fn check_part1_example2() {
-    assert_eq!(
-        solution(
-            1,
-            "..F7.
-.FJ|.
-SJ.L7
-|F--J
-LJ..."
-                .into()
-        ),
-        8
+        374
     );
 }
 
 #[test]
 fn check_part1() {
-    assert_eq!(solution(1, None), 6846);
-}
-
-#[test]
-fn check_part2_example1() {
-    assert_eq!(
-        solution(
-            2,
-            "...........
-.S-------7.
-.|F-----7|.
-.||.....||.
-.||.....||.
-.|L-7.F-J|.
-.|..|.|..|.
-.L--J.L--J.
-..........."
-                .into()
-        ),
-        4
-    )
-}
-
-#[test]
-fn check_part2_example2() {
-    assert_eq!(
-        solution(
-            2,
-            ".F----7F7F7F7F-7....
-.|F--7||||||||FJ....
-.||.FJ||||||||L7....
-FJL7L7LJLJ||LJ.L-7..
-L--J.L7...LJS7F-7L7.
-....F-J..F7FJ|L7L7L7
-....L7.F7||L7|.L7L7|
-.....|FJLJ|FJ|F7|.LJ
-....FJL-7.||.||||...
-....L---J.LJ.LJLJ..."
-                .into()
-        ),
-        8
-    )
-}
-
-#[test]
-fn check_part2_example3() {
-    assert_eq!(
-        solution(
-            2,
-"FF7FSF7F7F7F7F7F---7
-L|LJ||||||||||||F--J
-FL-7LJLJ||||||LJL-77
-F--JF--7||LJLJ7F7FJ-
-L---JF-JLJ.||-FJLJJ7
-|F|F-JF---7F7-L7L|7|
-|FFJF7L7F-JF7|JL---7
-7-L-JL7||F7|L7F-7F7|
-L.L7LFJ|||||FJL7||LJ
-L7JLJL-JLJLJL--JLJ.L"
-                .into()
-        ),
-        10
-    )
+    assert_eq!(solution(1, None), 9599070);
 }
 
 #[test]
 fn check_part2() {
-    // assert_eq!(solution(2, None), 0);
-    panic!();
+    assert_eq!(solution(2, None), 842645913794);
 }
